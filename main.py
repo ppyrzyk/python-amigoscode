79fffbf3f67998f64ee6e3b999c7f2fbef471c8a
ppyrzyk <115031262+ppyrzyk@users.noreply.github.com>
2023-04-13 21:39:05 +0200

    test

---
+++ main.py
@@ -0,0 +1,79 @@
+
+# print("Hello to python")
+
+# name = "Jamila"
+# age: int = 20
+# print(age)
+
+# name, age = "Pawel", 20
+#
+# print(name, age)
+
+# name = "James"
+# fullName = " J James"
+# PI = 3.14 # Uppercase name for variables tht do not change
+# print(PI)
+
+# brand = "AmigosCode"
+# age = 20
+# numbers = []
+# isAdult = False
+# print(type(brand))
+# print(type(age))
+# print(type(numbers))
+# print(type(isAdult))
+
+# brand: str = "AmigosCode" # you can specify what type it is but it isnt necessary
+# isAdult: bool = False
+# def hello() -> int: # specyfying what does methpd return but its isnt necessary
+#     return 1
+
+""" long comment can
+be written between
+3 double quotes
+on each side
+"""
+import keyword
+
+# brand = "Amigoscode"
+#
+# print("code" in brand) #return boolean true  or false
+# print("code" not in brand)
+
+# comment = """ you can write a long
+# variable and
+# it prints out in the same
+# paragraphs """
+#
+# print(comment)
+#
+# name = "Pawel"
+# age = 35
+# email = f"""
+# Hello {name},
+# I know yor are {age} years old
+# Best Regards {name.upper()} """
+#
+# print(email)
+
+# # resrved keywords
+# print(keyword.kwlist)
+
+# print(1+2)
+# print(10 - 3)
+# print(3*3)
+# print(3/3)
+# print(3**2)
+# print(10 % 6)
+
+# print(10 > 5)
+# print(10 >= 5)
+# print(10 < 5)
+# print(10 <= 5)
+# print(10 != 5)
+# print(10 == 10)
+
+
+# import calculator
+#
+# print(calculator.add(2,2))
